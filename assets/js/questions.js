8baa0d9225106e6e78eb935492f71f52c389e37a
Hpro97 <haxmo25@gmail.com>
2024-01-12 21:36:24 +0000

    added all js base files

---
+++ assets/js/questions.js
@@ -0,0 +1,24 @@
+// ---------------------------------
+// functionality:
+
+// main page has view highscores and start quizz button
+// when start quizz clicked then dispaly question with possible answers
+//     each answer has button
+//     event timer start if start button clicked
+//     event first question appears if start button clicked
+//     event next question appears if answer button clicked after 1 sec
+//     event text displays if correct
+//     event text displays if wrong
+//         event if wrong answer will take off time of timer
+//     event if all questions answered or timer runs out displays score
+//     event displays enter inital box
+//         if enter initials button clicked
+//             submit data (maybe make 4 char max)
+//             event if enter initials clicked displays our score in leaderboard
+//     if leaderboard clicked sorts  by highest first
+//         go back button takes you to mainpage
+//         clear high scores button deletes scored data and resets
+
+// hints: check js linked to html
+
+// ---------------------------------
